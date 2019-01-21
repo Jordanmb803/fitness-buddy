fae7599947113b1fd9b41848c56c141b0f3386f2
Jordan Buonforte <jordanbuonforte@Jordans-MacBook-Pro-2.local>
2019-01-21 15:56:10 -0700

    added user_id column to journal entries table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190120195528) do
+ActiveRecord::Schema.define(version: 20190121225436) do
 
   create_table "exercises", force: :cascade do |t|
     t.integer  "journal_entry_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20190120195528) do
     t.date     "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
 end
