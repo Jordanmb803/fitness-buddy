2a59ad2ac6d9cd3fe7aa66695705888c30706569
Jordan Buonforte <jordanbuonforte@Jordans-MacBook-Pro-2.local>
2019-02-16 23:42:04 -0700

    removed users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190121225436) do
+ActiveRecord::Schema.define(version: 20190217064045) do
 
   create_table "exercises", force: :cascade do |t|
     t.integer  "journal_entry_id"
