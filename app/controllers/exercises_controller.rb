a9fe98c79bd12eb609de126864fada022734e560
Jordan Buonforte <jordanbuonforte@Jordans-MacBook-Pro-2.local>
2019-02-20 20:32:40 -0700

    Added edit page

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -22,6 +22,10 @@ class ExercisesController < ApplicationController
     @exercise.save
   end
 
+  def edit
+    @exercise = Exercise.find(params[:id])
+  end
+
   def update
     @exercise = Exercise.find(params[:id])
     @exercise.update(exercise_params)
