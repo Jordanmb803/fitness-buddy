56c34fd63a7f6070e318a60d9115f8c266d2adf7
Jordan Buonforte <jordanbuonforte@Jordans-MacBook-Pro-2.local>
2019-03-08 12:08:21 -0700

    cleaned more code

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -5,14 +5,12 @@ class JournalEntriesController < ApplicationController
     if current_user.nil?
       redirect_to new_user_session_path
     else
-      @journal_entry = JournalEntry.find(params[:id])
-      @exercises     =   Exercise.where(journal_entry_id: @journal_entry.id)
-      @food_entries  =   FoodEntry.where(journal_entry_id: @journal_entry.id)
-      @calorie_total =   @food_entries.sum(:calories_per_serving)
-      @calorie_total ||= 0
-      @daily_calorie_goal = User.find(current_user.id).daily_calorie_goal
-      @daily_calorie_goal ||= 2000
-      @calories_left = @daily_calorie_goal - @calorie_total
+      @journal_entry      = JournalEntry.find(params[:id])
+      @exercises          = Exercise.where(journal_entry_id: @journal_entry.id)
+      @food_entries       = FoodEntry.where(journal_entry_id: @journal_entry.id)
+      @calorie_total      = calorie_total(@food_entries)
+      @daily_calorie_goal = daily_calorie_goal
+      @calories_left      = @daily_calorie_goal - @calorie_total
     end 
   end
   
@@ -36,4 +34,13 @@ class JournalEntriesController < ApplicationController
     journal_entry = JournalEntry.find_by(date: date, user_id: current_user.id)
     journal_entry ||= JournalEntry.create(date: date, user_id: current_user.id)
   end
+
+  def calorie_total food_entries
+    calorie_total = food_entries.sum(:calories_per_serving)
+    calorie_total ||= 0
+  end
+
+  def daily_calorie_goal
+    daily_calorie_goal = User.find(current_user.id).daily_calorie_goal
+    daily_calorie_goal ||= 2000
 end
